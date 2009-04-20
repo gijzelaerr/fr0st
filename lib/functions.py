e5d6cd33408a7c2cf41a5b03d025b498e83d31ad
Vitor Bosshard <algorias@gmail.com>
2009-04-20 20:54:24 +0000

    Transferred some contents of functions.py to display.py, preparing for  cleanup
    git-svn-id: https://fractalfrost.svn.sourceforge.net/svnroot/fractalfrost/trunk@62 24c11add-7f9c-4b28-9183-29c26a6ab64e

--- lib/functions.py
+++ lib/functions.py
@@ -14,31 +14,7 @@ sys.dont_write_bytecode = False # Why is this line here?
 
 import fr0stlib
 from fr0stlib import Flame, Xform
-from display import Display
 
-try:
-    import pygame
-    from pygame import (KMOD_ALT,KMOD_CAPS,KMOD_CTRL,KMOD_LALT,KMOD_LCTRL,
-KMOD_LMETA,KMOD_LSHIFT,KMOD_META,KMOD_MODE,KMOD_NONE,KMOD_NUM,KMOD_RALT,
-KMOD_RCTRL,KMOD_RMETA,KMOD_RSHIFT,KMOD_SHIFT,K_0,K_1,K_2,K_3,K_4,K_5,K_6,K_7,
-K_8,K_9,K_AMPERSAND,K_ASTERISK,K_AT,K_BACKQUOTE,K_BACKSLASH,K_BACKSPACE,K_BREAK,
-K_CAPSLOCK,K_CARET,K_CLEAR,K_COLON,K_COMMA,K_DELETE,K_DOLLAR,K_DOWN,K_END,
-K_EQUALS,K_ESCAPE,K_EURO,K_EXCLAIM,K_F1,K_F10,K_F11,K_F12,K_F13,K_F14,K_F15,
-K_F2,K_F3,K_F4,K_F5,K_F6,K_F7,K_F8,K_F9,K_FIRST,K_GREATER,K_HASH,K_HELP,K_HOME,
-K_INSERT,K_KP0,K_KP1,K_KP2,K_KP3,K_KP4,K_KP5,K_KP6,K_KP7,K_KP8,K_KP9,
-K_KP_DIVIDE,K_KP_ENTER,K_KP_EQUALS,K_KP_MINUS,K_KP_MULTIPLY,K_KP_PERIOD,
-K_KP_PLUS,K_LALT,K_LAST,K_LCTRL,K_LEFT,K_LEFTBRACKET,K_LEFTPAREN,K_LESS,K_LMETA,
-K_LSHIFT,K_LSUPER,K_MENU,K_MINUS,K_MODE,K_NUMLOCK,K_PAGEDOWN,K_PAGEUP,K_PAUSE,
-K_PERIOD,K_PLUS,K_POWER,K_PRINT,K_QUESTION,K_QUOTE,K_QUOTEDBL,K_RALT,K_RCTRL,
-K_RETURN,K_RIGHT,K_RIGHTBRACKET,K_RIGHTPAREN,K_RMETA,K_RSHIFT,K_RSUPER,
-K_SCROLLOCK,K_SEMICOLON,K_SLASH,K_SPACE,K_SYSREQ,K_TAB,K_UNDERSCORE,K_UNKNOWN,
-K_UP,K_a,K_b,K_c,K_d,K_e,K_f,K_g,K_h,K_i,K_j,K_k,K_l,K_m,K_n,K_o,K_p,K_q,K_r,
-K_s,K_t,K_u,K_v,K_w,K_x,K_y,K_z)
-except ImportError:
-##    raise
-    print("Warning: Couldn't import pygame.")
-    # Ugly hack or clever trick? you decide!
-    pygame = False
 
 #-------------------------------------------------------------------------------
 
@@ -101,14 +77,3 @@ def load_flames(filename,*args):
         flames.append(Flame(string=strings[key]))
         
     return flames
-
-        
-#------------------------------------------------------------------------------
-
-display = Display()
-keydown = display.keydown
-
-try:
-    display.init()
-except:
-    print("Warning: Can't initialize the display")
