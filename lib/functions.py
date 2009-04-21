e5fa554dd4137259ddb078bd2c0a8e03cc6ad98c
John Miller <john.jj.miller@gmail.com>
2009-04-21 22:55:24 +0000

    Improved the random palette generator while keeping it a "rotating" palette. Could possibly generalize further to work with a gui. Compacted all of the spline and vector generation into three functions:  - basic interpolator that does curves between points with cardinal spline smoothing overtop.  - helper function for point interpolation that supports polar and linear methods (basically a wrapper for 2-tuple interp)  - helper function for color interp (basically a wrapper for 3-tuple interp) Fleshed out the list of curves:  - lin - basic linear  - par - parabola  - cos - half a period of a cos wave  - sinh - hyperbolic sin normalized  - tanh - hyperbolic tan normalized
    I've decided to allow float palettes so that there's less rounding. 
    Colors should be rounded at the last possible moment (i.e. before write 
    out or display). flam3 supports floats for colors in xml.
    
    
    
    git-svn-id: https://fractalfrost.svn.sourceforge.net/svnroot/fractalfrost/trunk@80 24c11add-7f9c-4b28-9183-29c26a6ab64e

--- lib/functions.py
+++ lib/functions.py
@@ -62,9 +62,8 @@ def hls2rgb(color):
     h,l,s = color
     h = clip(h,0,1,True)
 
-    (r,g,b) = colorsys.hls_to_rgb(h,l,s)
-
-    return (r*255,b*255,g*255)
+    (r,b,g) = colorsys.hls_to_rgb(h,l,s)
+    return (r*255,g*255,b*255)
 
 #-------------------------------------------------------------------------------
 """
@@ -72,98 +71,106 @@ drange - Returns a list of values from x to y over n steps
   x - starting value
   y - ending value
   n - number of steps (returned list is n+1 items)
-  curve - lin, par
+  curve - lin, par, cos, sinh, tanh
+  a - curve param
 """
 def drange(x, y, n, curve='lin', a=1):
     v = []
     if curve=='par':
         d = (y-x)/(a*float(n)**2)
-        for i in range(0, n+1):
-            v.append(x + a*d*(i**2))
+        for i in xrange(0,n+1):
+            v.append(x+a*d*(i**2))
+    elif curve=='cos':
+        d = y-x
+        for i in xrange(0,n+1):
+            v.append((x+d*((cos(pi + (i/float(n))*pi))+1)/2)**a)
+    elif curve=='sinh':
+        d = y-x
+        for i in xrange(0,n+1):
+            v.append(x+d*(sinh(i/float(n))/sinh(1)))
+    elif curve=='tanh':
+        d = y-x
+        for i in xrange(0,n+1):
+            v.append(x+d*(tanh(i/float(n))/tanh(1)))
     else:
         d = (y-x)/float(n)
-        for i in xrange(0, n+1):
+        for i in xrange(0,n+1):
             v.append(x+d*i)
     return v
 
 """
-Smooth - Returns a smoothed curve from a list of 4 control points
+vector - Returns a cardinal spline from a list of 4 control points, or if less than 4 linear
   cps - list of control points (4 required - one before and one after the two that are being smoothed)
   n - distance between control points
+  curve - curve of the line between the two points to be smoothed (lin, par, cos)
+  a - curve parameter
   t - tension of the spline (0.5 = Catmull-Rom)
 """
-def smooth(cps, n, t=0.5):
-    y0 = cps[1]                     #initial vector location is second control point
-    y1 = cps[2]                     #ending location of the vector
-    
-    dy = y1-y0                      #delta between start and end
-    v = drange(0,1,n)
-    cps = numpy.array(cps)
-    M = numpy.array(
-                    [[0,1,0,0]
-                    ,[-t,0,t,0]
-                    ,[2*t,t-3,3-2*t,-t]
-                    ,[-t,2-t,t-2,t]]
-                    )
-    W = []
-    
-    for i in xrange(0,n+1):
-        vtmp = []
-        for j in xrange(0,4):
-            vtmp.append(v[i]**j)
-        W.append(vtmp)
-
-    W = numpy.array(W)
-    vp = numpy.dot(M,cps)
-    pk = numpy.dot(W,vp)
-    return pk
-    
-"""
-Interp - Returns a vector between two control points (p is for points and c is for colors)
-  cp1 - first control point
-  cp2 - second control point
-  n - distance between control points
-  curve - lin, par
-"""
-def interp(cp1, cp2, n, curve='lin'):
-    d = cp2 - cp1
-    v = drange(0,1,n,curve)
+def vector(cps, n, curve='lin', a=1, t=0.5):
     pk = []
-    for i in xrange(0, n+1):
-        pk.append(cp1+v[i]*d)
+    if len(cps)>1 and len(cps)<4:
+        d = cps[1] - cps[0]
+        v = drange(0,1,n,curve,a)
+        for i in xrange(0, n+1):
+            pk.append(cps[0]+v[i]*d)
+    elif len(cps)>=4:
+        dy = cps[2]-cps[1]
+        v = drange(0,1,n,curve,a)
+        cps = numpy.array(cps)
+        M = numpy.array([[0,1,0,0]
+                        ,[-t,0,t,0]
+                        ,[2*t,t-3,3-2*t,-t]
+                        ,[-t,2-t,t-2,t]]
+                        )
+        W = []
+        
+        for i in xrange(0,n+1):
+            vtmp = []
+            for j in xrange(0,4):
+                vtmp.append(v[i]**j)
+            W.append(vtmp)
+
+        W = numpy.array(W)
+        vp = numpy.dot(M,cps)
+        pk = numpy.dot(W,vp)
     return pk
 
 """
-space - rect or polar
+pinterp - Helper for point interpolation
+  space - rect or polar
 """    
-def pinterp(cp1, cp2, n, curve='lin', space='rect'):
+def pinterp(cps, n, curve='lin', space='rect'):
     if space=='polar':
-        cp1 = polar(cp1)
-        cp2 = polar(cp2)
-        px = interp(cp1[0], cp2[0], n, curve)
-        py = interp(cp1[1], cp2[1], n, curve)
+        for i in cps:
+            i = polar(i)
+        px = vector(cps, n, curve)
+        py = vector(cps, n, curve)
         pk = []
         for i in xrange(0,n+1):
             pk.append(rect((px[i],py[i])))
     else:
-        px = interp(cp1[0], cp2[0], n, curve)
-        py = interp(cp1[1], cp2[1], n, curve)
+        px = vector(cps, n, curve)
+        py = vector(cps, n, curve)
         pk = []
         for i in xrange(0,n+1):
             pk.append((px[i],py[i]))
     return pk
 
-def cinterp(c1, c2, n, curve='lin'):
-    if curve=='par':
-        rmid = abs((c2[0]-c1[0])/2.0)
-        gmid = abs((c2[1]-c1[1])/2.0)
-        bmid = abs((c2[2]-c1[2])/2.0)
-        r1 = interp(c1[0],rmid,n,'par')
-        g1 = interp(c1[1],gmid,n,'par')
-        b1 = interp(c1[2],bmid,n,'par')
-        r2 = interp(c2[0],rmid,n,'par')
-        g2 = interp(c2[1],gmid,n,'par')
-        b2 = interp(c2[2],bmid,n,'par')
+"""
+cinterp - Helper for interpolating colors
+"""
+def cinterp(cps, n, curve='cos'):
+"""
+    if curve=='par' and len(cps)<4: # special case for parabolic smoothing - kinda legacy w/ cos
+        rmid = abs((cps[1][0]-cps[0][0])/2.0)
+        gmid = abs((cps[1][1]-cps[0][1])/2.0)
+        bmid = abs((cps[1][2]-cps[0][2])/2.0)
+        r1 = vector([cps1[0],rmid],n,curve)
+        g1 = vector([cps1[1],gmid],n,curve)
+        b1 = vector([cps1[2],bmid],n,curve)
+        r2 = vector([cps2[0],rmid],n,curve)
+        g2 = vector([cps2[1],gmid],n,curve)
+        b2 = vector([cps2[2],bmid],n,curve)
         r2.reverse()
         g2.reverse()
         b2.reverse()
@@ -171,26 +178,30 @@ def cinterp(c1, c2, n, curve='lin'):
         g = g1[:-1] + g2
         b = b1[:-1] + b2
     else:
-        r = interp(c1[0], c2[0], n, curve)
-        g = interp(c1[1], c2[1], n, curve)
-        b = interp(c1[2], c2[2], n, curve)
+"""
+    r = vector(cps, n, curve)
+    g = vector(cps, n, curve)
+    b = vector(cps, n, curve)
     pk = []    
     for i in xrange(0,len(r)):
-        v.append((r[i],g[i],b[i]))
+        pk.append((r[i],g[i],b[i]))
     return pk
     
 """
 from_seed - Returns a palette from a seed color
+  seed - seed color
+  csplit - dist from compliment wheel's other side is (can be neg)
+  split - dist from seed rspl and lspl are
+  dist - distance from compliment in palette index
+  curve - type of palette smoothing (splines are not recommended at this time)
 """
-def from_seed(self, seed, split=20, dist=64, smoothed=False):
+def from_seed(seed, csplit=0, split=30,  dist=64, curve='lin'):
     (h,l,s) = rgb2hls(seed)
     split /= 360.0
-    comp = hls2rgb((h+0.5,l,s))
-    lspl = hls2rgb((h+0.5-split,l,s))
-    rspl = hls2rgb((h+0.5+split,l,s))
-
-    if smoothed: curve = 'par'
-    else:        curve = 'lin'
+    csplit /= 360.0
+    comp = hls2rgb((h+csplit+0.5,l,s))
+    lspl = hls2rgb((h-split,l,s))
+    rspl = hls2rgb((h+split,l,s))
 
     #g1 is from 0 (compliment) to dist (left split)
     g1 = cinterp(comp, lspl, dist, curve)
@@ -201,8 +212,12 @@ def from_seed(self, seed, split=20, dist=64, smoothed=False):
     #g4 is from 255-dist to 255
     g4 = cinterp(rspl, comp, dist, curve)
     
-    g = g1[:-1]+g2[1:-1]+g3[:1]+g4
-    return g
+    g1 = g1[:-1]
+    g2 = g2[:-1]
+    g3 = g3[1:]
+    g4 = g4[1:]
+    
+    return g1+g2+g3+g4
 
 def save_flame(filename,flame):
     save_flames(filename,flame)
