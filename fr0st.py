511de69e2efafe83f9eecdd4f8c7f912d2ecdfbf
Vitor Bosshard <algorias@gmail.com>
2009-04-30 20:17:56 +0000

    Removed fr0st.py, renamed gui.py to fr0st.py.
    git-svn-id: https://fractalfrost.svn.sourceforge.net/svnroot/fractalfrost/trunk@138 24c11add-7f9c-4b28-9183-29c26a6ab64e

--- fr0st.py
+++ fr0st.py
@@ -1,34 +1,9 @@
-#Copyright (c) 2008 Vitor Bosshard
-#This program licensed under the GPL. See license.txt for details.
-#
-#Tested under:
-#Python 2.6.1
-#Pygame 1.8.1.win32-py2.5
-#-----------------------------------------------------------------
+import wx
+app = wx.App(0)
+from lib.gui import MainWindow
 
-import os,sys
 
-sys.path.append(os.getcwd())
-
-if '.zip' in sys.path[0]: # Just for py2exe
-    # Keep the zipfile on the path
-    sys.path.append(sys.path[0])
-    # Use abspath because an exact match is needed for comparisons
-    newpath = os.path.join(sys.path[0],'..','..','scripts')
-    sys.path[0] = os.path.abspath(newpath)
-    # Py2exe won't find these imports otherwise.
-    if False: import default,pygame,ctypes,Queue
-else:
-    sys.path[0] = os.path.join(sys.path[0],"scripts")
-    
-    
-sys.dont_write_bytecode = True
-
-if len(sys.argv) is 1:
-    print('No script given as argument, loading default.py')
-    sys.argv.append('default')
-
-try:
-    __import__(sys.argv[1])
-except ImportError:
-    raise ImportError, "Unable to load script %s" %modname
+if __name__ == '__main__':
+##    app = wx.App(0) # 0 makes stderr go to the console. For debugging!
+    frame = MainWindow(None, wx.ID_ANY)
+    app.MainLoop()
--- gui.py
+++
@@ -1,9 +0,0 @@
-import wx
-app = wx.App(0)
-from lib.gui import MainWindow
-
-
-if __name__ == '__main__':
-##    app = wx.App(0) # 0 makes stderr go to the console. For debugging!
-    frame = MainWindow(None, wx.ID_ANY)
-    app.MainLoop()
