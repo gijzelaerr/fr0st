8b819f0e91781b268f2082c63b17bf70428e5ec2
John Miller <john.jj.miller@gmail.com>
2009-05-05 22:44:41 +0000

    Added ability to select which variants and what order they should appear in the var tab.
    git-svn-id: https://fractalfrost.svn.sourceforge.net/svnroot/fractalfrost/trunk@170 24c11add-7f9c-4b28-9183-29c26a6ab64e

--- lib/gui/xformeditor.py
+++ lib/gui/xformeditor.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 from decorators import Bind,BindEvents
 from lib.fr0stlib import polar, rect
 from lib import pyflam3
+import config
 
 
 def LoadIcon(name):
@@ -290,7 +291,7 @@ class VarPanel(wx.Panel):
 
         self.root = self.tree.AddRoot("The Root Item")
 
-        for i in pyflam3.variation_list:
+        for i in config.ACTIVE_VARS:
             child = self.tree.AppendItem(self.root, i)
 
             for j in pyflam3.variables[i]:
