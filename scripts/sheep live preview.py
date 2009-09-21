d9c18fa37e3ec8a9044d91eb846ded200a397d12
Vitor Bosshard <algorias@gmail.com>
2009-09-21 14:22:56 +0000

    Renamed and improved the sheep preview script. Still under testing.
    git-svn-id: https://fractalfrost.svn.sourceforge.net/svnroot/fractalfrost/trunk@375 24c11add-7f9c-4b28-9183-29c26a6ab64e

---
+++ scripts/sheep live preview.py
@@ -0,0 +1,45 @@
+''' "It's aliiiiive!" he screamed, as the rotating sheep
+mutated in realtime before his very eyes.'''
+
+import time
+
+def ScriptHack(func):
+    # First, hack our way around some "limitations". I want to keep
+    # all changes confined to this script, at least for now.
+    # Don't do this at home.
+
+    # Prevent normal updates to the preview window.
+    global preview
+    preview = self.image.RenderPreview
+    self.image.RenderPreview = lambda *a, **k: None
+
+    # Make the GUI believe the script is not running, so it'll
+    # allow us to modify things on the canvas.
+    self.scriptrunning = False
+
+    # On to the actual script
+    try:
+        func()
+    finally:
+        # Undo the hackery. Stopping the script raises an
+        # exception, so we need the finally clause.
+        self.image.RenderPreview = preview
+        preview()
+
+
+
+def main():
+    """This is the actual script."""
+    rot = 0
+    while True:
+        rot = (rot-5) % 360
+        f = flame.copy()
+        for x in f.xform:
+            if x.symmetry <= 0:
+                x.rotate(rot)
+        preview(f)
+        time.sleep(0.05)
+
+
+if __name__ == "__main__":
+    ScriptHack(main)
