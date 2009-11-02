ca9e0858c81422d02621d7ab2a95ee74557fd668
Vitor Bosshard <algorias@gmail.com>
2009-11-02 03:27:15 +0000

    Made test check for both to and from string performance.
    git-svn-id: https://fractalfrost.svn.sourceforge.net/svnroot/fractalfrost/trunk@616 24c11add-7f9c-4b28-9183-29c26a6ab64e

--- scripts/tests/_test_load_flame_benchmark.py
+++ scripts/tests/_test_load_flame_benchmark.py
@@ -3,8 +3,14 @@ import time
 s = flame.to_string()
 
 t = time.time()
-
 for i in range(1000):
     Flame(s)
 
-print time.time() - t
+print "fm", time.time() - t
+
+
+t = time.time()
+for i in range(1000):
+    flame.to_string()
+
+print "to", time.time() - t
