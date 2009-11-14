214dda2b495c3e7f487290f6a61321e410a8176b
Vitor Bosshard <algorias@gmail.com>
2009-11-14 15:57:48 -0300

    Updated random script.

--- scripts/random_flame.py
+++ scripts/random_flame.py
@@ -1,54 +1,27 @@
-from fr0stlib.functions import randrange2
-from fr0stlib import Flame, Xform
+import random
 
-def GenRandomBatch(numrand,*a, **k):
+from utils import calculate_colors, batch
 
-    lst = []
     
-    if len(a)==0:
-        raise ValueError, "number of xform config specs must be > 0"
-
-    if 'numbasic' in k and k['numbasic']>0 and len(a)>1:
-        print "more than one xform config spec specified for basic mode, using only first one"
-
-    for i in range(numrand):
-        f = GenRandomFlame(*a, **k)
-        f.name = "random_flame_%03d" % i
-        lst.append(f)
-
-    return lst    
-    
-def GenRandomFlame(*a,**k):
-
-    if 'numbasic' in k:
-        nb = k['numbasic']
-        nxforms=randrange2(2,nb+1,int=int)
-    else:
-        nb=0
-        nxforms = len(a)
-
+def GenRandom(*args):
     f = Flame()
-        
-    for i in range(nxforms):
-        if (nb>0):
-            Xform.random(f,col=float(i)/(nxforms-1),**a[0])        
-        else:
-            Xform.random(f,col=float(i)/(nxforms-1),**a[i])
-    
+    for a in args:
+        chance = a.get("chance", 1)
+        if random.random() < chance:
+            Xform.random(f, **a)
+            
     f.reframe()
+    calculate_colors(f)
     f.gradient.random(hue=(0, 1),saturation=(0, 1),value=(.25, 1),nodes=(4, 6))
     
     return f
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     randopt = [ { 'xv':range(1,6), 'n':2, 'xw':0},
                 { 'xv':range(20,26), 'n':2, 'xw':0},
-                { 'xv':range(6,9), 'n':1, 'fx':.5} ]
+                { 'xv':range(6,9), 'n':1, 'fx':True, "chance":.5} ]
                 
-    
-    batchsize=20
-    
-    lst = GenRandomBatch(batchsize,*randopt,numbasic=3)
-    save_flames("parameters/random_batch.flame",*lst)
+    lst = batch(GenRandom, 20, *randopt)
+    save_flames("parameters/random_batch.flame", *lst)
 
