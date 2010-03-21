04dd9887a05083cd0ebe7514ac2ecbf8143707cb
Vitor Bosshard <algorias@gmail.com>
2010-03-21 14:17:21 -0400

    Refactored configdlg so that the logic for the preview quality tab is in its own panel.

--- fr0stlib/gui/configdlg.py
+++ fr0stlib/gui/configdlg.py
@@ -39,7 +39,7 @@ class ConfigDialog(wx.Dialog):
         self.controls = {}
 
         notebook = wx.Notebook(self, style=wx.BK_DEFAULT)
-        notebook.AddPage(self.CreatePreviewSettings(notebook),
+        notebook.AddPage(PreviewPanel(notebook),
                          'Preview Quality', select=True)
         notebook.AddPage(RenderPanel(notebook), 'Renderer')
 
@@ -64,83 +64,6 @@ class ConfigDialog(wx.Dialog):
         self.Parent.canvas.ShowFlame(rezoom=False)
         self.Parent.image.RenderPreview()
         self.Parent.previewframe.RenderPreview()
-
-
-    def CreatePreviewSettings(self, parent):
-        panel = wx.Panel(parent)
-        gbs = wx.GridBagSizer(5, 5)
-        gbs.Add(self.CreateVariationPreviewSettings(panel), (0, 0), flag=wx.EXPAND)
-        gbs.Add(self.CreateSmallPreviewSettings(panel), (1, 0), (1, 1), flag=wx.EXPAND)
-        gbs.Add(self.CreateLargePreviewSettings(panel), (0, 1), (2, 1))
-        panel.SetSizerAndFit(gbs)
-        return panel
-        
-
-    def number_text(self, parent, sizer, row, label, config_section, config_key, min, max, is_int=False):
-        ntc = NumberTextCtrl(parent, min, max)
-        
-        if is_int:
-            ntc.MakeIntOnly()
-
-        self.controls[config_section + '->' + config_key] = ntc
-
-        section = self.local_config[config_section]
-        ntc.SetFloat(section[config_key])
-
-        def cb(tempsave=False):
-            section[config_key] = ntc.GetFloat()
-        ntc.callback = cb
-
-        sizer.Add(wx.StaticText(parent, label=label), (row, 0), flag=wx.ALIGN_CENTER_VERTICAL)
-        sizer.Add(ntc, (row, 1))
-        
-    def CreateVariationPreviewSettings(self, parent):
-        gbs = wx.GridBagSizer(5, 5)
-        gbs.AddGrowableCol(0)
-
-        self.number_text(parent, gbs, 0, 'Scale', 
-                'Var-Preview-Settings', 'range', 0.1, 5)
-        
-        self.number_text(parent, gbs, 1, 'Quality', 
-                'Var-Preview-Settings', 'numvals', 10, 40, is_int=True)
-
-        self.number_text(parent, gbs, 2, 'Depth',
-                'Var-Preview-Settings', 'depth', 1, 5, is_int=True) 
-
-        return Box(parent, 'Variation Preview', (gbs, 0, wx.EXPAND))
-
-    def CreateSmallPreviewSettings(self, parent):
-        gbs = wx.GridBagSizer(5, 5)
-        gbs.AddGrowableCol(0)
-
-        self.number_text(parent, gbs, 0, 'Quality', 
-                'Preview-Settings', 'quality', 1, 20, is_int=True)
-
-        self.number_text(parent, gbs, 1, 'Density Estimator', 
-                'Preview-Settings', 'estimator', 0, 20, is_int=True)
-
-        self.number_text(parent, gbs, 2, 'Filter Radius', 
-                'Preview-Settings', 'filter_radius', 0, 10)
-
-        return Box(parent, 'Preview', (gbs, 0, wx.EXPAND))
-
-    def CreateLargePreviewSettings(self, parent):
-        gbs = wx.GridBagSizer(5, 5)
-        gbs.AddGrowableCol(0)
-
-        self.number_text(parent, gbs, 0, 'Quality', 
-                'Large-Preview-Settings', 'quality', 1, 1000, is_int=True)
-
-        self.number_text(parent, gbs, 1, 'Density Estimator', 
-                'Large-Preview-Settings', 'estimator', 0, 20, is_int=True)
-
-        self.number_text(parent, gbs, 2, 'Filter Radius', 
-                'Large-Preview-Settings', 'filter_radius', 0, 10)
-
-        self.number_text(parent, gbs, 3, 'Oversample', 
-                'Large-Preview-Settings', 'spatial_oversample', 0, 5, is_int=True)
-
-        return Box(parent, 'Large Preview', (gbs, 0, wx.EXPAND))
 
 
 
@@ -169,3 +92,83 @@ class RenderPanel(wx.Panel):
         self.parent.local_config["renderer"] = self.rb.GetStringSelection()
         
 
+class PreviewPanel(wx.Panel):
+
+    def __init__(self, parent):
+        self.parent = parent.Parent
+        wx.Panel.__init__(self, parent, -1)
+        gbs = wx.GridBagSizer(5, 5)
+        gbs.Add(self.CreateVariationPreviewSettings(self.parent), (0, 0), flag=wx.EXPAND)
+        gbs.Add(self.CreateSmallPreviewSettings(self.parent), (1, 0), (1, 1), flag=wx.EXPAND)
+        gbs.Add(self.CreateLargePreviewSettings(self.parent), (0, 1), (2, 1))
+        self.SetSizerAndFit(gbs)
+
+
+
+    def CreateVariationPreviewSettings(self, parent):
+        gbs = wx.GridBagSizer(5, 5)
+        gbs.AddGrowableCol(0)
+
+        number_text(self, parent, gbs, 0, 'Scale', 
+                'Var-Preview-Settings', 'range', 0.1, 5)
+        
+        number_text(self, parent, gbs, 1, 'Quality', 
+                'Var-Preview-Settings', 'numvals', 10, 40, is_int=True)
+
+        number_text(self, parent, gbs, 2, 'Depth',
+                'Var-Preview-Settings', 'depth', 1, 5, is_int=True) 
+
+        return Box(self, 'Variation Preview', (gbs, 0, wx.EXPAND))
+
+    def CreateSmallPreviewSettings(self, parent):
+        gbs = wx.GridBagSizer(5, 5)
+        gbs.AddGrowableCol(0)
+
+        number_text(self, parent, gbs, 0, 'Quality', 
+                'Preview-Settings', 'quality', 1, 20, is_int=True)
+
+        number_text(self, parent, gbs, 1, 'Density Estimator', 
+                'Preview-Settings', 'estimator', 0, 20, is_int=True)
+
+        number_text(self, parent, gbs, 2, 'Filter Radius', 
+                'Preview-Settings', 'filter_radius', 0, 10)
+
+        return Box(self, 'Preview', (gbs, 0, wx.EXPAND))
+
+    def CreateLargePreviewSettings(self, parent):
+        gbs = wx.GridBagSizer(5, 5)
+        gbs.AddGrowableCol(0)
+
+        number_text(self, parent, gbs, 0, 'Quality', 
+                'Large-Preview-Settings', 'quality', 1, 1000, is_int=True)
+
+        number_text(self, parent, gbs, 1, 'Density Estimator', 
+                'Large-Preview-Settings', 'estimator', 0, 20, is_int=True)
+
+        number_text(self, parent, gbs, 2, 'Filter Radius', 
+                'Large-Preview-Settings', 'filter_radius', 0, 10)
+
+        number_text(self, parent, gbs, 3, 'Oversample', 
+                'Large-Preview-Settings', 'spatial_oversample', 0, 5, is_int=True)
+
+        return Box(self, 'Large Preview', (gbs, 0, wx.EXPAND))
+
+    
+
+def number_text(panel, parent, sizer, row, label, config_section, config_key, min, max, is_int=False):
+    ntc = NumberTextCtrl(panel, min, max)
+    
+    if is_int:
+        ntc.MakeIntOnly()
+
+    parent.controls[config_section + '->' + config_key] = ntc
+
+    section = parent.local_config[config_section]
+    ntc.SetFloat(section[config_key])
+
+    def cb(tempsave=False):
+        section[config_key] = ntc.GetFloat()
+    ntc.callback = cb
+
+    sizer.Add(wx.StaticText(panel, label=label), (row, 0), flag=wx.ALIGN_CENTER_VERTICAL)
+    sizer.Add(ntc, (row, 1))
