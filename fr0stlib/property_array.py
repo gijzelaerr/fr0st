c67c68e75321f0e48589f9bb7eb2d69e98594e15
Vitor Boshard <algorias@gmail.com>
2010-06-26 23:36:35 -0400

    Add GPL header, tidy up the code a bit.

--- fr0stlib/property_array.py
+++ fr0stlib/property_array.py
@@ -1,13 +1,31 @@
-import collections, sys
+##############################################################################
+#  Fractal Fr0st - fr0st
+#  https://launchpad.net/fr0st
+#
+#  Copyright (C) 2009 by Vitor Bosshard <algorias@gmail.com>
+#
+#  Fractal Fr0st is free software; you can redistribute
+#  it and/or modify it under the terms of the GNU General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Library General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this library; see the file COPYING.LIB.  If not, write to
+#  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+#  Boston, MA 02111-1307, USA.
+##############################################################################
+import collections, sys, numpy
 
-import numpy as N
 
-
-class _property_array(N.ndarray):
+class _property_array(numpy.ndarray):
     def __new__(cls, parent, instance, data):
-        obj = N.asarray(data).view(cls)
+        obj = numpy.asarray(data).view(cls)
         def callback():
-            print "callback called"
             parent.fset(instance, obj)
         obj.callback = callback
         return obj
@@ -20,19 +38,18 @@ class _property_array(N.ndarray):
 
 
     def __setitem__(self, pos, val):
-        N.ndarray.__setitem__(self, pos, val)
+        numpy.ndarray.__setitem__(self, pos, val)
         self.callback()
 
 
     def __eq__(self, other):
-        return all(N.equal(self, other))
+        return all(numpy.equal(self, other))
 
 
     def __ne__(self, other):
         return not self == other
     
 
-    
 
 class property_array(property): 
     def __init__(self, fget, fset=None, fdel=None, fdoc=None):
