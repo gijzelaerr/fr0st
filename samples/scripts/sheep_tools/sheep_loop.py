341abfdbef615b99c9c27c4e42eecc8f12c2548a
vitor <vitor@bosshard>
2010-09-01 18:47:54 +0200

    Refactored sheep_loop script, removed kludgy __init__.py file.

--- samples/scripts/sheep_tools/sheep_loop.py
+++ samples/scripts/sheep_tools/sheep_loop.py
@@ -1,4 +1,16 @@
-from sheep_tools import sheep_loop
-    
+
+def sheep_loop(flame, nframes):
+    name = flame.name + " %03d"
+    lst = []
+    for i in range(nframes):
+        for x in flame.xform:
+            if x.animate:
+                x.rotate(-360./nframes)
+        flame.name = name % i
+        lst.append(flame.copy())
+    return lst
+
+
 if __name__ == '__main__':
-    save_flames("parameters/sheep_loop.flame", *sheep_loop(flame, 120))
+    save_flames("parameters/sheep_loop.flame", *sheep_loop(flame, 120),
+                confirm=False)
