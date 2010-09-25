07f1cae121ec27629d38592af5be63edc9f2d91e
vitor <vitor@bosshard>
2010-09-25 09:28:19 +0200

    First step towards gradient editor (WIP).

---
+++ fr0stlib/gui/gradientbrowser.py
@@ -0,0 +1,52 @@
+import os, re, wx
+import xml.etree.cElementTree as etree
+import wx.combo
+from  wx.lib.filebrowsebutton import FileBrowseButton
+
+from fr0stlib import Palette, load_flamestrings
+from fr0stlib.gui.utils import Box
+from fr0stlib.gui.config import config
+
+
+class GradientBrowser(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, -1)
+
+        mask = "Flame Files (*.flame)|*.flame|" \
+               "Gradient Files (*.ugr)|*.ugr|" \
+               "Map Files (*.map)|*.map"
+
+        path = os.path.abspath(config["flamepath"])
+        self.fbb = FileBrowseButton(self, -1, fileMask=mask,
+                                    labelText='File:', initialValue=path)
+             
+        self.bcb = wx.combo.BitmapComboBox(self, style=wx.CB_READONLY)
+
+        self.load(path)
+
+        szr = Box(self, "Gradient Browser", (self.fbb, 0, wx.EXPAND|wx.ALL, 5),
+                                            (self.bcb, 0, wx.EXPAND|wx.ALL, 5))
+        self.SetSizerAndFit(szr)
+
+
+    def load(self, path):
+        self.palettes = []
+        # TODO: clear bcb
+
+        for name, palette in self.from_file(path):
+            img = wx.ImageFromBuffer(256, 1, palette.to_buffer())
+            img.Rescale(128, 20)
+            bmp = wx.BitmapFromImage(img)
+            self.bcb.Append(name, bmp)
+            self.palettes.append(palette)
+
+
+    def from_file(self, path):
+        ext = os.path.splitext(path)[1]
+        if ext == ".flame":
+            return ((re.search(' name="(.*?)"', string).group(1),
+                     Palette(etree.fromstring(string)))
+                    for string in load_flamestrings(path))
+
+
+
