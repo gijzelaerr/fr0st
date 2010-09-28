66b21d8422a29b61e6572260a3e47ad1407537b6
vitor <vitor@bosshard>
2010-09-28 09:30:00 +0200

    gradient browser now working, but only with .flame files.

--- fr0stlib/gui/gradientbrowser.py
+++ fr0stlib/gui/gradientbrowser.py
@@ -11,6 +11,7 @@ from fr0stlib.gui.config import config
 class GradientBrowser(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, -1)
+        self.parent = parent
 
         mask = "Flame Files (*.flame)|*.flame|" \
                "Gradient Files (*.ugr)|*.ugr|" \
@@ -18,9 +19,11 @@ class GradientBrowser(wx.Panel):
 
         path = os.path.abspath(config["flamepath"])
         self.fbb = FileBrowseButton(self, -1, fileMask=mask,
-                                    labelText='File:', initialValue=path)
+                                    labelText='File:', initialValue=path,
+                                    changeCallback=self.fbb_callback)
              
         self.bcb = wx.combo.BitmapComboBox(self, style=wx.CB_READONLY)
+        self.Bind(wx.EVT_COMBOBOX, self.OnCombo, self.bcb)
 
         self.load(path)
 
@@ -31,17 +34,19 @@ class GradientBrowser(wx.Panel):
 
     def load(self, path):
         self.palettes = []
-        # TODO: clear bcb
+        self.bcb.Clear()
 
-        for name, palette in self.from_file(path):
+        for name, palette in self.parse_file(path):
             img = wx.ImageFromBuffer(256, 1, palette.to_buffer())
             img.Rescale(128, 20)
             bmp = wx.BitmapFromImage(img)
             self.bcb.Append(name, bmp)
             self.palettes.append(palette)
 
+        self.bcb.Select(0)
 
-    def from_file(self, path):
+
+    def parse_file(self, path):
         ext = os.path.splitext(path)[1]
         if ext == ".flame":
             return ((re.search(' name="(.*?)"', string).group(1),
@@ -49,4 +54,11 @@ class GradientBrowser(wx.Panel):
                     for string in load_flamestrings(path))
 
 
+    def OnCombo(self, e):
+        self.parent.parent.flame.gradient[:] = self.palettes[e.Int][:]
+        self.parent.parent.TempSave()
 
+
+    def fbb_callback(self, e):
+        self.load(e.String)
+        
