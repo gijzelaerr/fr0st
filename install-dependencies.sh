9c5e6e1fe0e6982357c18b6f0df51ccb218206cb
algorias@gmail.com <>
2010-12-19 11:53:00 +0100

    Changed aptitude to apt-get in shell command, since the former doesn't seem to come preinstalled anymore with the latest ubuntu.

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -21,7 +21,7 @@
 #  Boston, MA 02111-1307, USA.
 ##############################################################################
 
-sudo aptitude install build-essential libtool autoconf libpng12-dev libjpeg62-dev libxml2-dev python-dev python-numpy python-wxgtk2.8 subversion
+sudo apt-get install build-essential libtool autoconf libpng12-dev libjpeg62-dev libxml2-dev python-dev python-numpy python-wxgtk2.8 subversion
 #svn co http://flam3.svn.sourceforge.net/svnroot/flam3/branches/early-clip/src flam3-2.8-src
 svn co http://flam3.googlecode.com/svn/trunk/src flam3-2.8-src
 
